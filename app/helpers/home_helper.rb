c59900ad6edfab9c13c6dc9d954ddf2720ed04e3
Yuriy <yuriy.dobryanskiy@gmail.com>
2016-09-08 13:22:47 +0300

    add contact us screen

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,23 +1,2 @@
 module HomeHelper
-
-  # def link_earth_globe
-  #   content_tag :span, class: 'earth-globe-container' do
-  #     html = link_to(
-  #       content_tag(:span, nil, class: 'glyphicon earth-globe'),
-  #       '#', class: 'icon', rel: 'popover', tabindex: '0',
-  #       data: {
-  #         toggle: 'popover',
-  #         turbolinks: false
-  #       }
-  #     )
-  #     html += content_tag :div, class: 'hide earth-globe-popover-content' do
-  #       content_tag :ul do
-  #         concat content_tag(:li, link_to(t('language.english'), '#', class: 'active'))
-  #         concat content_tag(:li, link_to(t('language.spanish'), '#'))
-  #         concat content_tag(:li, link_to(t('language.german'), '#'))
-  #         concat content_tag(:li, link_to(t('language.russian'), '#'))
-  #       end
-  #     end
-  #   end
-  # end
 end

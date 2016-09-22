b4e5dd66ef88c3b7f753a1060f868b446c7c7c99
Yuriy <yuriy.dobryanskiy@gmail.com>
2016-09-22 14:56:42 +0300

    update smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,9 +92,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
-    port:                 465,
-    ssl:                  true,
-    domain:               'gmail.com',
+    port:                 587,
+    domain:               'innovationfeel.com',
     user_name:            settings['mailer']['user_name'],
     password:             settings['mailer']['password'],
     authentication:       'plain',

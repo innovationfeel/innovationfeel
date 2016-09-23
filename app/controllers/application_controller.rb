49d80b738d72dd58f80e5d09cd224cf149eff510
Yuriy <yuriy.dobryanskiy@gmail.com>
2016-09-23 13:58:40 +0300

    fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session, only: :json_request?
   before_action :set_locale
 
   private
@@ -7,5 +8,9 @@ class ApplicationController < ActionController::Base
   def set_locale
     I18n.locale =
       %w(en es de ru).include?(params[:locale]) ? params[:locale] : I18n.default_locale
+  end
+
+  def json_request?
+    request.format.json?
   end
 end
